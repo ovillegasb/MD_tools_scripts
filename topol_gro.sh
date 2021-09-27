8397cb6c4765427b59630b25ef598f2f954ce851
ovillegas <orlando.villegas@univ-pau.fr>
2021-09-28 00:17:51 +0200

    Correction path to itp file

--- topol_gro.sh
+++ topol_gro.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # file mol itp name (only name)
-
 mol=$1
+
+# number of molecules for topology
+num=$2
 
 # Topology System File
 echo -e "\e[1;34mWrtiting Topology File \e[0m"
@@ -14,8 +16,8 @@ echo "; System Topology File" >> topol.top
 echo "; orlando.villegas@univ-pau.fr" >> topol.top
 echo -e ";\n" >> topol.top
 echo -e "#include \"oplsaa.ff/forcefield.itp\"\n" >> topol.top
-echo -e "#include \"../atomstypes.itp\"\n" >> topol.top
-echo -e "#include \"../${mol}.itp\"\n" >> topol.top
+# echo -e "#include \"../atomstypes.itp\"\n" >> topol.top
+echo -e "#include \"${mol}.itp\"\n" >> topol.top
 echo -e "[ system ]\nSystem\n" >> topol.top
 echo -e "[ molecules ]" >> topol.top
 printf "%-5s %-5s\n" ${mol^^} $num >> topol.top
